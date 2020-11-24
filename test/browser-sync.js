958b3b05709a07ba961edd9c46338400b957985b
davcd <hello@davcd.com>
2020-11-24 23:16:31 +0100

    fix: added resume.json to browser-sync watcher

--- test/browser-sync.js
+++ test/browser-sync.js
@@ -1,15 +1,15 @@
 const bs = require('browser-sync').create();
 const fs = require('fs')
 const index = require('../index');
-const resume = require('../resume.json');
 
 const buildHtml = (cb) => {
+    const resume = JSON.parse(fs.readFileSync('./resume.json', 'utf-8'))
     let output = index.render(resume)
     fs.writeFileSync('./public/index.html', output, {flag: 'w'})
     cb()
 }
 
-bs.watch("./assets/**").on('change', () => buildHtml(() => bs.reload()))
+bs.watch(["./assets/**", "./resume.json"]).on('change', () => buildHtml(() => bs.reload()))
 
 buildHtml(() =>
     bs.init({
