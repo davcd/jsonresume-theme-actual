46050661c8f7dde9787e2389598005d2457fd8ce
davcd <hello@davcd.com>
2020-11-27 20:57:17 +0100

    refactor: basics section optimizations

--- assets/helper.js
+++ assets/helper.js
@@ -1,9 +1,27 @@
 const md = require('markdown-it')();
 
-const mdToHtml = (string) =>{
-    return md.render(string)
+const mdToHtml = (string) => md.render(string)
+
+const calcLocation = (location) => {
+    if (location.countryCode) {
+        if (location.region) {
+            return `${location.region}, ${location.countryCode}`
+        } else if (location.city) {
+            return `${location.city}, ${location.countryCode}`
+        }
+        return location.countryCode
+    } else if (location.region) {
+        if (location.city) {
+            return `${location.city}, ${location.region}`
+        }
+        return location.region
+    } else if (location.city) {
+        return location.city
+    }
+    return null
 }
 
 module.exports = {
-    mdToHtml
+    mdToHtml,
+    calcLocation
 }
