b9f2f7fa6454258cb6d5e9ea8f45b416384a33da
davcd <hello@davcd.com>
2020-12-03 22:33:10 +0100

    feature: added skills, education, certificates, languages and links support

--- assets/helper.js
+++ assets/helper.js
@@ -1,7 +1,9 @@
 const md = require('markdown-it')();
 const iso = require('iso-3166-1');
+const moment = require('moment');
 
-const mdToHtml = (string) => md.render(string)
+
+const mdToHtml = (string) => string ? md.render(string) : ''
 
 const calcLocation = (location) => {
     let array = []
@@ -16,13 +18,46 @@ const calcLocation = (location) => {
 
     if (location.countryCode) {
         const country = iso.whereAlpha2(location.countryCode)
-        array.push(country && array.length<2 ? country.country : location.countryCode)
+        array.push(country && array.length < 2 ? country.country : location.countryCode)
     }
 
     return array.length > 0 ? array.join(', ') : null
 }
 
+const calcDateRange = (start, end) => {
+    let array = []
+
+    if (start) {
+        array.push(beautifyDate(start))
+    }
+
+    array.push(end ? beautifyDate(end) : 'Present')
+
+    return array.join(' - ')
+}
+
+const beautifyDate = (date) => moment(date, 'YYYY-MM-DD').format('MMM YYYY')
+
+const beautifyArray = (separator, array) => array.filter(x => x).join(separator)
+
+const beautifyLink = (string) => {
+    let s = string.trim().replace(/^(?:https?:\/\/)?(?:www\.)?/i, '')
+    s = s.endsWith('/') ? s.slice(0, -1) : s
+    s = s.split('/')
+    s[0] = `<strong>${s[0]}</strong>`
+    return s.join('/')
+}
+
+
+const validArray = (array) => array !== undefined && array.length > 0
+
+
 module.exports = {
     mdToHtml,
-    calcLocation
+    calcLocation,
+    calcDateRange,
+    beautifyDate,
+    beautifyArray,
+    beautifyLink,
+    validArray
 }
--- test/helper.test.js
+++ test/helper.test.js
@@ -3,8 +3,12 @@ const helper = require('../assets/helper');
 
 describe('mdToHtml function', () => {
 
-    test('given null then throw Error', () => {
-        expect(() => helper.mdToHtml(null)).toThrow(Error)
+    test('given null then return empty string', () => {
+        expect(helper.mdToHtml(null)).toBe('')
+    })
+
+    test('given undefined then return empty string', () => {
+        expect(helper.mdToHtml(undefined)).toBe('')
     })
 
     test('given empty string then return empty string', () => {
@@ -63,12 +67,12 @@ describe('calcLocation function', () => {
         expect(helper.calcLocation(location)).toBe('Santander, Spain')
     })
 
-    test('given city and countryCode then return city, country', () => {
+    test('given region and countryCode then return region, country', () => {
         const location = {
-            "city": "Santander",
+            "region": "Cantabria",
             "countryCode": "ES"
         }
-        expect(helper.calcLocation(location)).toBe('Santander, Spain')
+        expect(helper.calcLocation(location)).toBe('Cantabria, Spain')
     })
 
     test('given city and region then return city, region', () => {
@@ -88,4 +92,95 @@ describe('calcLocation function', () => {
         expect(helper.calcLocation(location)).toBe('Santander, Cantabria, ES')
     })
 
+})
+
+describe('calcDateRange function', () => {
+
+    test('given undefined and undefined then return Present', () => {
+        expect(helper.calcDateRange(undefined, undefined)).toBe('Present')
+    })
+
+    test('given startDate and undefined then return startDate - Present', () => {
+        expect(helper.calcDateRange('2019-01-20', undefined)).toBe('Jan 2019 - Present')
+    })
+
+    test('given undefined and endDate then return endDate', () => {
+        expect(helper.calcDateRange(undefined, '2020-01-20')).toBe('Jan 2020')
+    })
+
+    test('given startDate and endDate then return startDate - endDate', () => {
+        expect(helper.calcDateRange('2019-01-20', '2020-01-20')).toBe('Jan 2019 - Jan 2020')
+    })
+
+    test('given invalid date and invalid date then return Invalid date - Invalid date', () => {
+        expect(helper.calcDateRange('2009-20-222', '20020-14-50')).toBe('Invalid date - Invalid date')
+    })
+})
+
+describe('beautifyDate function', () => {
+
+    test('given valid date then return beautified date', () => {
+        expect(helper.beautifyDate('2020-01-20')).toBe('Jan 2020')
+    })
+
+    test('given invalid date then return null', () => {
+        expect(helper.beautifyDate('')).toBe('Invalid date')
+    })
+
+    test('given undefined date then return null', () => {
+        expect(helper.beautifyDate(undefined)).toBe('Invalid date')
+    })
+
+})
+
+describe('beautifyArray function', () => {
+
+    test('given empty array then return empty string', () => {
+        expect(helper.beautifyArray(', ', [])).toBe('')
+    })
+
+    test('given single element array date then return element', () => {
+        expect(helper.beautifyArray(' - ', ['test'])).toBe('test')
+    })
+
+    test('given two element array then beautified list', () => {
+        expect(helper.beautifyArray(';', ['test1', 'test2'])).toBe('test1;test2')
+    })
+
+    test('given two element array being one undefined then return valid element', () => {
+        expect(helper.beautifyArray('*', [undefined, 'test2'])).toBe('test2')
+    })
+
+})
+
+
+describe('beautifyLink', () => {
+
+    test('given domain return strong domain', () => {
+        expect(helper.beautifyLink('yourwebsite.dev/')).toBe('<strong>yourwebsite.dev</strong>')
+    })
+
+    test('given url www, domain and path then return strong domain, and path', () => {
+        expect(helper.beautifyLink('www.test.co.uk/route')).toBe('<strong>test.co.uk</strong>/route')
+    })
+
+    test('given url with protocol, www, domain and path then return strong domain, and path', () => {
+        expect(helper.beautifyLink('https://www.domain.com/path')).toBe('<strong>domain.com</strong>/path')
+    })
+})
+
+
+describe('validArray', () => {
+
+    test('given undefined return false', () => {
+        expect(helper.validArray(undefined)).toBe(false)
+    })
+
+    test('given empty array, return false', () => {
+        expect(helper.validArray([])).toBe(false)
+    })
+
+    test('given valid array, return true', () => {
+        expect(helper.validArray([1])).toBe(true)
+    })
 })
